fb09fa1199efad67179077d474249a498488b225
Tony Chen <tchen51@stuy.edu>
2025-03-30 23:44:34 -0400

    add basic request functionality

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+app = Flask(__name__)
+
+def num_rows():
+    con = sqlite3.connect("confessions.db")
+    cur = con.cursor()
+    cur.execute("select count(*) from post")
+    return cur.fetchone()[0]
+
+@app.route("/", methods=["POST", "GET"])
+def submit():
+    con = sqlite3.connect("confessions.db")
+    cur = con.cursor()
+    cur.execute("create table if not exists post(id, title, confession)")
+    if request.method == "POST":
+        conf_id = num_rows()
+        title = request.form.get("title")
+        conf = request.form.get("confession")
+        cur.execute("insert into post (id, title, confession) values (?, ?, ?)", (conf_id, title, conf))
+        con.commit()
+        return redirect(url_for("fetch"))
+    return render_template("submit.html")
+
+@app.route("/submissions", methods=["GET"])
+def fetch():
+    con = sqlite3.connect("confessions.db")
+    cur = con.cursor()
+    cur.execute("create table if not exists post(id, title, confession)")
+    res = cur.execute("select * from post")
+    confessions = res.fetchall()
+    return render_template("submissions.html", confessions=confessions)
+
