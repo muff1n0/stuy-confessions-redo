1868da1b17ca7fdb6a62fd00f4496088903c2baf
Tony Chen <tchen51@stuy.edu>
2025-03-31 23:28:28 -0400

    add navbar and basically everything

--- app.py
+++ app.py
@@ -9,26 +9,36 @@ def num_rows():
     cur.execute("select count(*) from post")
     return cur.fetchone()[0]
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/submit")
+def subpage():
+    return render_template("submit.html")
+
+@app.route("/submits", methods=["POST", "GET"])
 def submit():
     con = sqlite3.connect("confessions.db")
     cur = con.cursor()
-    cur.execute("create table if not exists post(id, title, confession)")
+    cur.execute("create table if not exists post(id, title, confession, upvotes)")
     if request.method == "POST":
         conf_id = num_rows()
         title = request.form.get("title")
         conf = request.form.get("confession")
-        cur.execute("insert into post (id, title, confession) values (?, ?, ?)", (conf_id, title, conf))
+        cur.execute("insert into post (id, title, confession, upvotes) values (?, ?, ?, ?)", (conf_id, title, conf, 0))
         con.commit()
         return redirect(url_for("fetch"))
     return render_template("submit.html")
 
-@app.route("/submissions", methods=["GET"])
+@app.route("/", methods=["GET"])
 def fetch():
     con = sqlite3.connect("confessions.db")
     cur = con.cursor()
-    cur.execute("create table if not exists post(id, title, confession)")
+    cur.execute("create table if not exists post(id, title, confession, upvotes)")
     res = cur.execute("select * from post")
     confessions = res.fetchall()
     return render_template("submissions.html", confessions=confessions)
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
